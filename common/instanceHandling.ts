eabd9b7750fa8518a4fb4d01026148513c701949
Iñaki Guastalli <iaguastalli@gmail.com>
2025-05-02 18:30:54 +0200

    updated to nextjs v12

--- common/instanceHandling.ts
+++ common/instanceHandling.ts
@@ -1,29 +1,34 @@
 import Config from "../domain/Config";
 
 export function getConfig(): Config {
-    const fs = require('fs');
-    let config: Config;
-    try {
-        const rawData = fs.readFileSync('./config.json');
-        config = JSON.parse(rawData);
-    } catch (error) {
-        throw new Error("Unable to properly parse configuration file. Make sure you have a config.json file on the app root folder and that it's a valid json.");
-    }
-    return config;
+  const fs = require("fs");
+  let config: Config;
+  try {
+    const rawData = fs.readFileSync("./config.json");
+    config = JSON.parse(rawData);
+  } catch (error) {
+    throw new Error(
+      "Unable to properly parse configuration file. Make sure you have a config.json file on the app root folder and that it's a valid json."
+    );
+  }
+  return config;
 }
 
 export function getFoundryPid(foundryAppBasePath: string): number | undefined {
-    const { execSync } = require("child_process");
-    //var isWin = process.platform === "win32";
-    // if (isWin) {
-    //     return 123; // FIXME how to do this on windows?!
-    // } else {
-    //     return execSync(`pgrep -f ${foundryAppBasePath}/resources/app/main.js`).toString().split('\n').filter(Boolean).map(Number);
-    // }
+  const { execSync } = require("child_process");
+  var isWin = process.platform === "win32";
 
-    try {
-        return execSync(`pgrep -f ${foundryAppBasePath}/resources/app/main.js`).toString().split('\n').filter(Boolean).map(Number);
-    } catch (error) {
-        return;
+  try {
+    if (isWin) {
+      throw new Error("Incompatible with win32");
+    } else {
+      return execSync(`pgrep -f ${foundryAppBasePath}/resources/app/main.js`)
+        .toString()
+        .split("\n")
+        .filter(Boolean)
+        .map(Number);
     }
+  } catch (error) {
+    return;
+  }
 }
--- pages/api/toggle-instance/[instanceId].ts
+++ pages/api/toggle-instance/[instanceId].ts
@@ -5,49 +5,55 @@ import { InstanceConfig } from "../../../domain/InstanceConfig";
 const { execSync, spawn } = require("child_process");
 
 function stopInstance(instancePid: number) {
-    execSync(`kill ${instancePid}`);
+  execSync(`kill ${instancePid}`);
 }
 
 function startInstance(instance: InstanceConfig): number {
-    console.log(`Starting instance ${instance.name} (id: ${instance.id})`);
-    const fvtt = spawn('node', [`${instance.foundry.app}/resources/app/main.js`, `--dataPath=${instance.foundry.data}`], {
-        detached: true,
-    });
-    return fvtt.id;
+  console.info(`Starting instance ${instance.name} (id: ${instance.id})`);
+  const fvtt = spawn(
+    "node",
+    [
+      `${instance.foundry.app}/resources/app/main.js`,
+      `--dataPath=${instance.foundry.data}`,
+    ],
+    {
+      detached: true,
+    }
+  );
+  return fvtt.id;
 }
 
-export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-    const { instanceId } = req.query;
-    console.log(`instanceId: [${instanceId}]`)
-    const instances: InstanceConfig[] = getConfig().instances;
-    let runningInstance: { instance: InstanceConfig, pid: number } | undefined;
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { instanceId } = req.query;
+  console.info(`instanceId: [${instanceId}]`);
+  const instances: InstanceConfig[] = getConfig().instances;
+  let runningInstance: { instance: InstanceConfig; pid: number } | undefined;
 
-    for (const instance of instances) {
-        const pid = getFoundryPid(instance.foundry.app);
-        if (pid) {
-            runningInstance = { instance, pid };
-            break;
-        }
+  for (const instance of instances) {
+    const pid = getFoundryPid(instance.foundry.app);
+    if (pid) {
+      runningInstance = { instance, pid };
+      break;
     }
+  }
 
-    const toToggle = instances.find(i => i.id == instanceId);
-    if (!toToggle) {
-        const message = `Unable to find instance with id [${instanceId}] for operation`;
-        console.error(message);
-        return res.status(403).json({ message: message });
-    }
+  const toToggle = instances.find((i) => i.id == instanceId);
+  if (!toToggle) {
+    const message = `Unable to find instance with id [${instanceId}] for operation`;
+    console.error(message);
+    return res.status(403).json({ message: message });
+  }
 
-    if (runningInstance) {
-        console.log(`Found running instance.. stopping [${runningInstance.instance.name}]`);
-        stopInstance(runningInstance.pid);
-    }
+  if (runningInstance) {
+    console.info(
+      `Found running instance.. stopping [${runningInstance.instance.name}]`
+    );
+    stopInstance(runningInstance.pid);
+  }
 
-    if (toToggle !== runningInstance?.instance) {
-        startInstance(toToggle);
-    }
+  if (toToggle !== runningInstance?.instance) {
+    startInstance(toToggle);
+  }
 
-    return res.status(200).json({ queryId: instanceId })
+  return res.status(200).json({ queryId: instanceId });
 }
